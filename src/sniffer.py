87bc2670f006aeae3cf8a543b247fe156e9c04e2
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2026-02-07 11:04:28 +0100

    feat: :sparkles: add logging to sniffer

--- src/sniffer.py
+++ src/sniffer.py
@@ -2,19 +2,29 @@ from scapy.all import *
 from scapy.layers.inet import IP, ICMP
 from scapy.layers.rip import RIP, RIPEntry, RIPAuth
 import argparse
+import logging
+
+def create_logger(name=__name__):
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.INFO)
+        formater = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formater)
+        logger.addHandler(console_handler)
+        return logger
 
 class Sniffer:
-    def __init__(self, interface):
+    def __init__(self, interface, logger = None | logging.Logger):
         self.interface = interface
+        self.logger = logger if logger else create_logger(self.__class__.__name__)
 
     def start(self):
-        # sniff(iface=self.interface, filter="icmp or icmp6 or rip", prn=self.process_packet)
         sniff(iface=self.interface, filter="udp port 520", prn=self.process_packet)
-        # sniff(iface=self.interface, prn=self.process_packet)
 
     def process_packet(self, packet):
-        # print(packet.show())
-        print(packet.summary())
+        rip_packet = packet[RIP]
+        self.logger.info(packet.summary())
+        self.logger.info(rip_packet.show())
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple packet sniffer using Scapy")
