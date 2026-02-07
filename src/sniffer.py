34a14f7aaa3f8c945dce7e7e970539e3fa3a2c22
Wiktor Sędzimir <wiktor.sedzimir@gmail.com>
2026-02-07 16:12:55 +0100

    refactor: :recycle: improve sniffer logging and typing

--- src/sniffer.py
+++ src/sniffer.py
@@ -6,17 +6,17 @@ import argparse
 import logging
 
 class Sniffer:
-    def __init__(self, interface, logger: logging.Logger | None = None):
+    def __init__(self, interface: str, logger: logging.Logger | None = None):
         self.interface = interface
-        self.logger = logger if logger else create_logger(self.__class__.__name__)
+        self.logger = logger if logger else create_logger(self.__class__.__name__, level=logging.DEBUG)
 
     def start(self):
         sniff(iface=self.interface, filter="udp port 520", prn=self.process_packet)
 
-    def process_packet(self, packet):
-        rip_packet = packet[RIP]
+    def process_packet(self, packet: Packet):
+        rip_packet: RIP = packet[RIP]
         self.logger.info(packet.summary())
-        self.logger.debug(rip_packet.show())
+        self.logger.debug(f"RIP packet:\n{rip_packet.show(dump=True)}")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Simple packet sniffer using Scapy")
